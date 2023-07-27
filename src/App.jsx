a54bfbdfea91285f94187cb20160b04113bfe1db
Kum Somi <kumsomi.5@gmail.com>
2023-07-27 14:40:58 +0530

    feat: page layout with routing of each page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Footer, Navbar } from "./component";
+import { Home } from "./page/home";
+import { Login } from "./page/auth/login";
+import { SignUp } from "./page/auth/signup";
+import { ProductListing } from "./page/productListing";
+import { Cart } from "./page/Cart";
+import { Wishlist } from "./page/wishlist";
 
 function App() {
   return (
     <div>
-      <Navbar/>
-      Body
-      <Footer/>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/products" element={<ProductListing />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
