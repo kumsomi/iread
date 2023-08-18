6367b8ef0a10585e001fe121439702d058569402
Kum Somi <kumsomi.5@gmail.com>
2023-08-18 23:47:43 +0530

    feat: created product card

--- src/component/product/productCard.jsx
+++ src/component/product/productCard.jsx
@@ -1,12 +1,38 @@
 import React from "react";
+import "./styles.css";
 import pic from "../../assests/images/olympics.jpg";
+import { AiOutlineHeart, AiFillStar } from "react-icons/ai";
 
 const ProductCard = ({ title, author, _id, categoryName, price }) => {
+  const specialTag = "Hardcover";
+  const originalPrice = 1490;
+  const discount = 57;
+  const rating = 4.5;
   return (
     <div className="product-card" key={_id}>
       <img className="product-img" src={pic} alt="olympics" />
-      <h4>{title}</h4> by {author} in category:
-      {categoryName} at $ {price}
+
+      <div className="product-title">{title}</div>
+      <div className="product-author">{author}</div>
+      <div className="product-price-tag">
+        <span className="product-discount-price">₹{price}</span>
+        <span className="product-original-price">₹{originalPrice}</span>
+      </div>
+      <span className="product-discount">{discount}%</span>
+
+      <div>
+        <span className="product-category">{categoryName}</span>
+      </div>
+      <div className="product-overhead">
+        <span className="special-tag">{specialTag}</span>
+        <span className="product-like-btn">
+          <AiOutlineHeart />
+        </span>
+      </div>
+      <div className="product-rating">
+        {rating} <AiFillStar />
+      </div>
+      <button className="add-cart-btn">Add to cart</button>
     </div>
   );
 };
